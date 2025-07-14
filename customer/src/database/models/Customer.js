d686c9150453f599fcf3edfbf23aab2a140c4858
salma <sa1963645@gmail.com>
2025-07-14 15:21:13 +0300

    Refactor Customer schema in database model to improve readability and consistency. Adjust formatting of address, cart, wishlist, and orders fields, and ensure proper use of require property.

--- customer/src/database/models/Customer.js
+++ customer/src/database/models/Customer.js
@@ -7,26 +7,35 @@ const CustomerSchema = new Schema({
     password: String,
     salt: String,
     phone: String,
-    address:[
+    address: [
         { type: Schema.Types.ObjectId, ref: 'address', require: true }
     ],
     cart: [
         {
-          product: { type: Schema.Types.ObjectId, ref: 'product', require: true},
-          unit: { type: Number, require: true}
+            product: {
+                _id: { type: String, require: true },
+                name: { type: String },
+                banner: { type: String },
+                price: { type: Number }
+            },
+            unit: { type: Number, require: true }
         }
     ],
-    wishlist:[
-        { 
-            type: Schema.Types.ObjectId, ref: 'product', require: true
+    wishlist: [
+        {
+            _id: { type: String, require: true },
+            name: { type: String },
+            description: { type: String },
+            banner: { type: String },
+            price: { type: Number },
         }
     ],
-    orders: [ 
+    orders: [
         { type: Schema.Types.ObjectId, ref: 'order', require: true }
     ]
-},{
+}, {
     toJSON: {
-        transform(doc, ret){
+        transform(doc, ret) {
             delete ret.password;
             delete ret.salt;
             delete ret.__v;
@@ -35,4 +44,4 @@ const CustomerSchema = new Schema({
     timestamps: true
 });
 
-module.exports =  mongoose.model('customer', CustomerSchema);
+module.exports = mongoose.model('customer', CustomerSchema);
