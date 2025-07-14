2e54eaeef1541ce331751b2f2bc1210c2e79bdbc
salma <sa1963645@gmail.com>
2025-07-14 16:48:51 +0300

    Update Customer schema to include 'available' field and modify 'orders' structure for improved data representation.

--- customer/src/database/models/Customer.js
+++ customer/src/database/models/Customer.js
@@ -27,11 +27,16 @@ const CustomerSchema = new Schema({
             name: { type: String },
             description: { type: String },
             banner: { type: String },
-            price: { type: Number },
+            available: { type: Boolean },
+            price: { type: Number }
         }
     ],
     orders: [
-        { type: Schema.Types.ObjectId, ref: 'order', require: true }
+        {
+            _id: { type: String, require: true },
+            amount: { type: String },
+            date: { type: Date, default: Date.now() }
+        }
     ]
 }, {
     toJSON: {
